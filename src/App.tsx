7a62fddb1bb1efe85474c1ef715370f705ff8dae
manikandan.a <maniavudai10@gmail.com>
2025-05-04 23:09:34 +0530

    include unused codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AboutUs from "./components/AboutUs";
+import Services from "./components/Services";
+import UniteWithUs from "./components/UniteWithUs";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import AboutUs from "./components/AboutUs";
-import UniteWithUs from "./components/UniteWithUs";
-import AnimatedSplitImages from "./components/AnimatedSplitImages";
-import Services from "./components/Services";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
+
       <Router>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -29,7 +24,6 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
-    </TooltipProvider>
   </QueryClientProvider>
 );
 
