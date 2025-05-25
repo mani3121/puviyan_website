cf7a1e94e98d7f4d3c56e4e96571bd076f9a9a90
manikandan.a <maniavudai10@gmail.com>
2025-05-26 00:25:03 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -67,22 +67,20 @@ const Index = () => {
   
 
           <section id="about-us" className="w-full bg-white">
-            <div className="w-full px-4 py-16">
+            <div className="w-full px-4 ">
               <AboutUs />
             </div>
           </section>
 
 
           {/* Separator Image between About Us and Unite with Us */}
-          <section id="separator-image" className="w-full bg-white">
-            <div className="w-full flex justify-center items-center py-8">
-              <img
-                src="https://puviyan-website.vercel.app/images/ribbon.jpg"
-                alt="Separator"
-                className="w-full max-w-4xl object-cover"
-                loading="lazy"
-              />
-            </div>
+          <section id="separator-image" className="w-full">
+            <img
+              src="https://puviyan-website.vercel.app/images/ribbon.jpg"
+              alt="Separator"
+              className="w-full object-cover"
+              loading="lazy"
+            />
           </section>
 
           <section id="unite-with-us" className="w-full bg-white">
