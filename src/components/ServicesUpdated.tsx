c99d50236fb6b4c509e9ef8b42815d753b4d2b84
manikandan.a <maniavudai10@gmail.com>
2025-05-26 23:16:28 +0530

    Update ServicesUpdated.tsx

--- src/components/ServicesUpdated.tsx
+++ src/components/ServicesUpdated.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const ServicesUpdated = () => (
   <div className="w-full min-h-screen flex items-center justify-center bg-white">
     <img
       src="https://puviyan-website.vercel.app/images/Services.gif"
       alt="Services"
-      className="max-w-full h-auto"
+      className="max-w-full h-auto transform -translate-y-12"
       loading="lazy"
     />
   </div>
